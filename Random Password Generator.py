aa459b6326325956995d01e12cf5f850551c8c99
Heco <99784203+Heco-o@users.noreply.github.com>
2022-07-14 16:42:42 +0600

    Update Random Password Generator.py

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -5,21 +5,29 @@ b = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s"
 c = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 d = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\\",":",";","\"","'","<",",",">",".","?","/"]
 password = ""
-lenth = int(input("How many charecters you want in your password?\n"))
-number = input("Do you want numbers in your password? Y/N\n").lower()
-if number == "yes" or number == "y":
+while True:
+    try:
+        lenth = int(input("How many characters do you want in your password?\n"))
+        if lenth <= 0:
+            print("Please enter only numbers that are larger than 0\n")
+        else:
+            break
+    except ValueError:
+        print("Please enter only numbers\n")
+number = input("Do you want any numbers in your password? Y/N\n").lower()
+if number == "yes" or number == "y" or number.startswith("y"):
 	for i in a:
 		A.append(i)
 uppercase = input("Do you want any uppercase letters in your password? Y/N\n").lower()
-if uppercase == "yes" or uppercase == "y":
+if uppercase == "yes" or uppercase == "y" or uppercase.startswith("y"):
 	for i in c:
 		A.append(i)
 lowercase = input("Do you want any lowercase letters in your password? Y/N\n").lower()
-if lowercase == "yes" or lowercase == "y":
+if lowercase == "yes" or lowercase == "y" or lowercase.startswith("y"):
 	for i in b:
 		A.append(i)
 special = input("Do you want any special characters in your password? Y/N\n").lower()
-if special == "yes" or special == "y":
+if special == "yes" or special == "y" or special.startswith("y"):
 	for i in d:
 		A.append(i)
 for i in range(lenth):
