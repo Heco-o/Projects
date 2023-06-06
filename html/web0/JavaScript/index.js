ec78ce1535523caea4d8ac1f7a8472c96075a89b
Heco-o <heco.html@gmail.com>
2023-06-06 23:17:12 +0600

    11:17/6/6/2023

--- html/web0/JavaScript/index.js
+++ html/web0/JavaScript/index.js
@@ -1,9 +1,4 @@
-let SRender = katex.renderToString;
-
 document.getElementById("button").onclick = function(){
-  let exprStr = document.getElementById("in").value;
-  let exprFn = numeric.parse(exprStr); // compile expression string into a function
-  let expr = exprFn({}); // evaluate the function with an empty object for variables
-  document.getElementById("Lab0").innerHTML = expr.toString();
-  document.getElementById("Lab").innerHTML = SRender(numeric.prettyPrint(expr));
+	let expr = new algebra.parse(document.getElementById("in").value);
+	document.getElementById("Lab").innerHTML = katex.renderToString(algebra.toTex(expr).toString());
 };
