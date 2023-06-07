e68dcc3191e3056bd4caf9002ab2f90571da81b2
Heco-o <heco.html@gmail.com>
2023-06-08 00:20:43 +0600

    date: 08/06/2023; time: 00:20:42

--- html/web0/JavaScript/index.js
+++ html/web0/JavaScript/index.js
@@ -1,4 +1,3 @@
 document.getElementById("button").onclick = function(){
-	let expr = new algebra.parse(document.getElementById("in").value);
-	document.getElementById("Lab").innerHTML = katex.renderToString(algebra.toTex(expr).toString());
+	document.getElementById("Lab").innerHTML = katex.renderToString(math.simplify(document.getElementById("in").value).toTex());
 };
--- nodejs/Node.js
+++ nodejs/Node.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const http = require('http');
-const { exec } = require('child_process');
+const {
+	spawn
+} = require('child_process');
 const socketIO = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Handle incoming WebSocket connections
 io.on('connection', (socket) => {
-  console.log('A new client connected');
+	console.log('A new client connected');
 
-  // Execute a command in Termux
-  socket.on('command', (command) => {
-    exec(command, (error, stdout, stderr) => {
-      // Send the command output back to the client
-      socket.emit('output', stdout);
-    });
-  });
+	// Create a new terminal session
+	const term = spawn('bash');
 
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A client disconnected');
-  });
+	// Send terminal output to the client
+	term.stdout.on('data', (data) => {
+		socket.emit('output', data.toString());
+	});
+
+	// Send terminal errors to the client
+	term.stderr.on('data', (data) => {
+		socket.emit('error', data.toString());
+	});
+
+	// Execute commands in the terminal
+	socket.on('command', (command) => {
+		term.stdin.write(command + '\n');
+	});
+
+	// Handle disconnection
+	socket.on('disconnect', () => {
+		console.log('A client disconnected');
+		term.kill();
+	});
 });
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
