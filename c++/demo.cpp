a746c042933872124c267564f3aa475e792a5317
Heco-o <heco.html@gmail.com>
2023-06-21 00:21:15 +0600

    date: 21/06/2023; time: 00:21:15

--- c++/demo.cpp
+++ c++/demo.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 
-int main() {
-	for(long double n = 1; n < n + 1; n++) {
-		long double a = 2;
-		long double b = 2;
-		long double c = 2;
-		long double d = 2;
-		long double result = a + b * (n - 1) + (c * (n - 1) * (n - 2)) / 2 + (d * (n - 1) * (n - 2) * (n - 3)) / 6;
-		std::cout << result << std::endl;
-	}
+int main(){
+	std::string name = "Hello" " World";
+	std::cout << name << std::endl;
 }
