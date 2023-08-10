97efb0e12b0747275a661cc7196565e89b3797f9
Heco-o <heco.html@gmail.com>
2023-08-11 01:15:05 +0600

    date: 11/08/2023; time: 01:15:03

--- python/Word Finder/test3.py
+++ python/Word Finder/test3.py
@@ -6,7 +6,7 @@ def most_common_letter_word(words_list):
 	word_counts = [Counter(set(word)) for word in words_list]
 	#print(word_counts, end ="\n\n")
 	combined_counts = sum(word_counts, Counter())
-	#print(combined_counts, end="\n\n")
+	print(combined_counts, end="\n\n")
 	
 	sorted_words = sorted(words_list, key=lambda w: sum(combined_counts[letter] for letter in set(w)), reverse=True)
 	return sorted_words
@@ -17,15 +17,16 @@ with open("Word_lengths.json", "r") as F:
 symbol = "-"
 end = []
 lenth = int(input("Enter the lenth of the pattern:\n"))
+words = Words[str(lenth)]
 pattern = symbol
 while symbol in pattern:
 	pattern = input("Enter the green letter:\n")
 	letters = list(input("Enter the yellow letter:\n"))
 	_end = list(input("Enter the black letter:\n"))
 	
-	for i in _end: end.append(i)
+	for i in _end:
+		end.append(i)
 	
-	words = Words[str(lenth)]
 	result = []
 	for word in words:
 		if all(pattern[i] == symbol or word[i] == pattern[i] for i in range(len(pattern))):
---
+++ rust/test0/target/debug/deps/test0-2655c9e61c5f4f69.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/test0-2655c9e61c5f4f69: src/main.rs
+
+/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/test0-2655c9e61c5f4f69.d: src/main.rs
+
+src/main.rs:
