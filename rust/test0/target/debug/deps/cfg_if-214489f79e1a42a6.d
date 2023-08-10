6dbf67960978817b28354124dcb4c026bc6e6359
Heco-o <heco.html@gmail.com>
2023-08-11 01:16:03 +0600

    date: 11/08/2023; time: 01:16:03

--- rust/test0/target/debug/deps/cfg_if-214489f79e1a42a6.d
+++
@@ -1,7 +0,0 @@
-/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/cfg_if-214489f79e1a42a6.rmeta: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/libcfg_if-214489f79e1a42a6.rlib: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/cfg_if-214489f79e1a42a6.d: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- rust/test0/target/debug/deps/test0-2655c9e61c5f4f69.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/test0-2655c9e61c5f4f69: src/main.rs
-
-/data/data/com.termux/files/home/github/Projects/rust/test0/target/debug/deps/test0-2655c9e61c5f4f69.d: src/main.rs
-
-src/main.rs:
